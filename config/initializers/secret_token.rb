83abe92a60eff0cc5177ca2e3ebf8e728c3690d7
dufresne@mojotech.com <dufresne@mojotech.com>
2014-02-04 14:33:53 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twin::Application.config.secret_token = '5a379e27974078067141e12bee7bb05740cef8b8a76bb0967d9a56e00d97bbf9ef1f03757fd0396f19dfb3814e2651f8ce7fbf7624614e876524aa4ea4e5e1ba'
